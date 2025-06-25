879ea587fe15d170162885bbc5e19daaf529e35e
Vladimir-Beliy <vovaazick@gmail.com>
2025-06-25 21:21:27 +0700

    Minor fix

--- contracts/utils/Roles.sol
+++ contracts/utils/Roles.sol
@@ -10,14 +10,14 @@ pragma solidity ^0.8.20;
  */
 library Roles {
     /// @dev Role that owns protocol fees and treasury funds.
-    bytes32 public constant FINANCIAL_ROLE = keccak256("FINANCIAL_ROLE");
+    bytes32 internal constant FINANCIAL_ROLE = keccak256("FINANCIAL_ROLE");
 
     /// @dev Whitelist role for regulated ArtToken transfers.
-    bytes32 public constant PARTNER_ROLE = keccak256("PARTNER_ROLE");
+    bytes32 internal constant PARTNER_ROLE = keccak256("PARTNER_ROLE");
 
     /// @dev Role allowed to sign off-chain permits and other authorizations.
-    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+    bytes32 internal constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
 
     /// @dev Role allowed to perform admin actions.
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 internal constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 }
