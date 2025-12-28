f46977c4040e554a9af092eae4ab2dd29b399539
mriynyk <vovaazick@gmail.com>
2025-12-28 17:29:50 +0700

    Update configuration

--- types/environment.ts
+++ types/environment.ts
@@ -1,44 +1,71 @@
-export type ChainConfig = {
+/* ################### Chain Config ############################# */
+export type ChainConfigYaml = {
+    [chainName: string]: ChainChainConfigYaml;
+};
+
+type ChainChainConfigYaml = {
     chainId: number;
     url: string;
     deployerPrivateKey: string;
     main: string;
 };
 
-export type CollectionConfig = CollectionData & {
+/* ################### Collection Config ############################# */
+
+export type CollectionConfigYaml = CollectionDataYaml & {
+    [chainName: string]: ChainCollectionConfigYaml;
+};
+
+type CollectionDataYaml = {
+    name: string;
+    symbol: string;
+};
+
+type ChainCollectionConfigYaml = {
     minAuctionDurationHours: number;
     artToken: UpgradeableContractConfig;
     auctionHouse: UpgradeableContractConfig;
 };
 
-export type MarketConfig = {
+/* ################### Market Config ############################# */
+
+export type MarketConfigYaml = {
+    [chainName: string]: ChainMarketConfigYaml;
+};
+
+type ChainMarketConfigYaml = {
     market: UpgradeableContractConfig;
 };
 
-export type ProtocolConfig = Pick<ChainConfig, 'main'> & {
+/* ################### Env Config ############################# */
+
+export type EnvConfigYaml = {
+    fork: {
+        name: string;
+        chainId: number;
+        url: string;
+    };
+    reportGas: boolean;
+    etherscanApiKey: string;
+    coinmarketcapApiKey: string;
+};
+
+/* ################### Formatted Configs ############################# */
+
+export type EnvConfig = EnvConfigYaml;
+export type ChainConfig = ChainChainConfigYaml;
+export type CollectionConfig = CollectionDataYaml & ChainCollectionConfigYaml;
+export type MarketConfig = ChainMarketConfigYaml;
+
+export type ProtocolConfig = Pick<ChainChainConfigYaml, 'main'> & {
     collection: CollectionConfig;
     market: MarketConfig;
 };
 
-export type ChainConfigTop = {
-    [chainName: string]: ChainConfig;
-};
-
-export type CollectionConfigTop = CollectionData & {
-    [chainName: string]: Omit<CollectionConfig, keyof CollectionData>;
-};
-
-export type MarketConfigTop = {
-    [chainName: string]: MarketConfig;
-};
+/* ################### Common Types ############################# */
 
 type UpgradeableContractConfig = {
     proxy: string;
     impl: string;
     admin: string;
-};
-
-type CollectionData = {
-    name: string;
-    symbol: string;
 };
