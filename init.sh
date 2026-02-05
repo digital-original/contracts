b13995b79559a7517f1677d83ef2783da7a3137d
mriynyk <vovaazick@gmail.com>
2026-02-05 18:28:57 +0700

    Fix workflows

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 test -f config.env.yaml || cp config.env.example.yaml config.env.yaml &&
 test -f config.chain.yaml || cp config.chain.example.yaml config.chain.yaml &&
-test -f config.collection.yaml || cp config.collections.example.yaml config.collections.yaml &&
-test -f config.market.yaml || cp config.markets.example.yaml config.markets.yaml
+test -f config.collection.yaml || cp config.collection.example.yaml config.collection.yaml &&
+test -f config.market.yaml || cp config.market.example.yaml config.market.yaml
